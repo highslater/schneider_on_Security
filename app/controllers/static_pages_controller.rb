3162e2540369cbf915c067050ac6ea78fbb8e2f1
highslater <highslater@hotmail.com>
2014-08-31 17:16:03 -0400

    Add a StaticPages controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,25 @@
+class StaticPagesController < ApplicationController
+  def blog
+  end
+
+  def newsletter
+  end
+
+  def books
+  end
+
+  def essays
+  end
+
+  def news
+  end
+
+  def schedule
+  end
+
+  def crypto
+  end
+
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 SchneierOnSecurity::Application.routes.draw do
+  get "static_pages/blog"
+  get "static_pages/newsletter"
+  get "static_pages/books"
+  get "static_pages/essays"
+  get "static_pages/news"
+  get "static_pages/schedule"
+  get "static_pages/crypto"
+  get "static_pages/about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

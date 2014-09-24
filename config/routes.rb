84669df0a6a6cdd5c658e996e5a81390e123a513
highslater <highslater@hotmail.com>
2014-09-24 16:48:45 -0400

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 SchneierOnSecurity::Application.routes.draw do
-  get "users/new"
+  resources                     :users
   root					'static_pages#blog'
-  match		'/newsletter',	to: 	'static_pages#newsletter', 	via: 	'get'
-  match		'/books',	to: 	'static_pages#books', 		via: 	'get'
-  match		'/essays',	to: 	'static_pages#essays', 		via: 	'get'
-  match		'/news',		to: 	'static_pages#news', 		via: 	'get'
-  match		'/schedule',	to: 	'static_pages#schedule', 	via: 	'get'
-  match		'/crypto',	to: 	'static_pages#crypto', 		via: 	'get'
-  match		'/about',	to: 	'static_pages#about', 		via: 	'get'
-  match 	'/signup',  	to: 	'users#new',            		via: 	'get'
+  match		'/newsletter',      to: 	'static_pages#newsletter', 	via: 	'get'
+  match		'/books',	        to: 	'static_pages#books', 		via: 	'get'
+  match		'/essays',	        to: 	'static_pages#essays', 		via: 	'get'
+  match		'/news',		        to: 	'static_pages#news', 		via: 	'get'
+  match		'/schedule',	        to: 	'static_pages#schedule', 	via: 	'get'
+  match		'/crypto',	        to: 	'static_pages#crypto', 		via: 	'get'
+  match		'/about',	        to: 	'static_pages#about', 		via: 	'get'
+  match 	'/registration',     to: 	'users#new',            		via: 	'get'
 end
